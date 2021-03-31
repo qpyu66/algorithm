04697aa7070fef6d01d7d728fa8a3104519d0703
Bae <qpyu66@naver.com>
2021-04-01 00:40:58 +0900

    lc_20_2

--- lc_20.py
+++ lc_20.py
@@ -1,6 +1,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        return 0
+#s="()[]"
+s="([)]"
+#s="(){()[{][]"
+slist = list(s)
+an =[]
+print(slist)
+  
+for i in s:
+    #print(i)
+    if i == '[' or i == '{' or i == '(' :
+        print('[i > ',i)
+        an.append(i)
+    #괄호 맞는 것 끼리 맨 뒤에서부터 pop      
+    elif i == ']' or i == '}' or i == ')':
+        an.pop()
+
+print('an> ', an)
+
+if len(an) != 0:
+    print('false')
+        return ""
+        
     
     
 s = Solution()
