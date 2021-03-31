4807cb33046639d6903cbe0083e2bca30e340ee5
Bae <qpyu66@naver.com>
2021-03-31 17:30:01 +0900

    leetcode 에서 오류

--- lc_14. Longest Common Prefix.py
+++ lc_14. Longest Common Prefix.py
@@ -1,4 +1,5 @@
 from typing import List
+from collections import OrderedDict
 
 
 class Solution:
@@ -6,28 +7,20 @@ class Solution:
         lslen = len(strs)
         sortedwords = sorted(strs, key=len)
         sortlen = len(sortedwords[0])
-        print(sortedwords, type(sortedwords), sortlen)
 
         oc = []
-        for s in range(lslen):
-            os = list(sortedwords[s])
-            oc.append(os)
+        for i in range(sortlen):
+            a = sortedwords[0][i]
+            for j in range(1, lslen):
+                b = sortedwords[j][i]
+                if a != b:
+                    oc.pop()
+                    break
 
-        oclen = len(oc)
-        alloc = []
-
-        for j in range(sortlen):
-            if (oc[0][j] == oc[1][j] == oc[2][j]):
-                print('ok', oc[0][j], oc[1][j], oc[2][j])
-                alloc.append(oc[0][j])
-                print(j)
-
-        alloc.append("")
-
-        return ''.join(alloc)
-
+                oc.append(a)
+        ans = ''.join(OrderedDict.fromkeys(oc))
+        return ans
 
 s = Solution()
-# strs = ['flower', 'flow', 'flight']
-strs = ['flower', 'flowes', 'floweds']
+strs = ["flower","flow","flight"]
 s.longestCommonPrefix(strs)
