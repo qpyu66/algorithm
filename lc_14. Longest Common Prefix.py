8e89e57f1e9811886fd7b6049078743158c8e780
Bae <qpyu66@naver.com>
2021-04-03 00:01:19 +0900

    lc_14 ok

--- lc_14. Longest Common Prefix.py
+++ lc_14. Longest Common Prefix.py
@@ -1,38 +1,57 @@
-from typing import List
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
 
+If there is no common prefix, return an empty string "".
+
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+0 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lower-case English letters.
+
+"""
+
+
+from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        lslen = len(strs)
-        print(lslen, strs, type(strs))
-        sortedwords = sorted(strs, key=len)
-        sortlen = len(sortedwords[0])
-        print('s> ',sortedwords,', ',sortlen)
         non = ""
+        #빈 리스트 체크
         if not strs:
             return non
         
-        oc =[]
-        for i in range(sortlen):
+        #제일 작은 문자열
+        mstrs = min(strs, key=len)
+        slen = len(mstrs)
+        #print("strs min> ", mstrs)
+       
+        #입력받은 리스트len 만큼
+        for i in range(slen):
             a = strs[0][i]
-            #print('i >',i,' ,a > ',a)
-            for j in range(1,lslen):
-                b = sortedwords[j][i]
-                #print(sortedwords[j][i], 'j >', j)            
-                if strs[0][i] != sortedwords[j][i]:
-                    #oc.append(non)
-                    return ""
-                    #break
-            print('a > ',a)        
-            oc.append(a)   
-            print('oc > ', oc)
-            ocjoin = ''.join(oc)    
-        print('ocjoin >',ocjoin)
-        
-        return ocjoin
+            #strs[0]와 비교를 위해 strs[i] i가 1 부터 증가하면서 비교 
+            for j in range(1, len(strs)):
+                if strs[j][i] != a: #문자 동일하지 않으면 빈값 입력
+                    return non
+            non = non + a #문자 더하기
             
+        return non
+        
 
 s = Solution()
-strs = ["flower","flow","flight"]
-#strs = ["dog","racecar","car"]
-s.longestCommonPrefix(strs)
+print(s.longestCommonPrefix(["flower","flow","flight"]))
+print(s.longestCommonPrefix(["dog","racecar","car"]))
+print(s.longestCommonPrefix([""]))
+print(s.longestCommonPrefix(["ab","a"]))
