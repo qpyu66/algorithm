44bb6db99c276002d8abe4e0f545b883256cbdb6
Bae <qpyu66@naver.com>
2021-04-12 23:16:01 +0900

    lc_58 ok

---
+++ leetcode_easy/lc_58._Length_of_Last_Word.py
@@ -0,0 +1,42 @@
+"""
+Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+
+A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Example 2:
+
+Input: s = " "
+Output: 0
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+
+"""
+
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        sp = s.split()
+        #print(sp,len(sp))        
+        if (len(sp) == 0):
+            #print('not')
+            return 0
+        else:
+            return len(sp[-1])
+
+        
+s = Solution()
+print(s.lengthOfLastWord("Hello World"))
+print(s.lengthOfLastWord(" "))
+print(s.lengthOfLastWord("Hello World si"))
+print(s.lengthOfLastWord("       "))
