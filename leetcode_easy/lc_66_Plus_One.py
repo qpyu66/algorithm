f3c42b2207baf9bcff48d9db34f5a2f5267b881b
Bae <qpyu66@naver.com>
2021-04-13 21:01:54 +0900

    lc_66 ok

--- leetcode_easy/lc_66_Plus_One.py
+++ leetcode_easy/lc_66_Plus_One.py
@@ -29,4 +29,26 @@ Constraints:
 0 <= digits[i] <= 9
 """
 
+from typing import List
 
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        si = [str(int) for int in digits]
+        djoin = ''.join(si)
+        #print(djoin)
+        #print(type(djoin))
+        dint = int(djoin)
+        print(dint, type(dint))
+        a = dint + 1
+        print(a,type(a))
+        return list(str(a))
+       
+        
+        
+s = Solution()
+print(s.plusOne([1,2,3]))
+print(s.plusOne([4,3,2,1]))
+print(s.plusOne([0]))
+print(s.plusOne([9]))
+print(s.plusOne([1,9]))
+print(s.plusOne([9,9])) #[1,0,0]
