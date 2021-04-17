edea015962c1e74cefa3718494f240f0e8cb89c1
Bae <qpyu66@naver.com>
2021-04-17 23:34:50 +0900

    lc_70 init

--- leetcode_easy/lc_67_Add_Binary.py
+++ leetcode_easy/lc_67_Add_Binary.py
@@ -25,6 +25,7 @@ Each string does not contain leading zeros except for the zero itself.
 #이진수->십진수
 #십진수 더하기
 #십진수->이진수
+#28ms
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         inta = int(a,2)
@@ -35,6 +36,65 @@ class Solution:
         return formab
         
         
+#code refactoring // 24ms
+# class Solution:
+#     def addBinary(self, a: str, b: str) -> str:
+#         num_a = int(a, 2)
+#         num_b = int(b, 2)
+#         return str(bin(num_a + num_b))[2:]
+
+
+#code refactoring // 20 ms
+# class Solution:
+#     def addBinary(self, a: str, b: str) -> str:
+#         result, carry = "", "0"
+#         i, j = len(a)-1, len(b)-1
+#         while i >= 0 and j >= 0:
+#             if a[i] == b[j] == "1":
+#                 result = carry + result
+#                 carry = "1"
+#             elif a[i] == b[j] == "0":
+#                 result = carry + result
+#                 carry = "0"
+#             elif carry == "0":
+#                 result = "1" + result
+#             else:
+#                 result = "0" + result
+#             i -= 1
+#             j -= 1
+                
+#         while i >= 0:
+#             if a[i] == carry =="1":
+#                 result = "0" + result
+#                 carry = "1"
+#             elif a[i] == carry =="0":
+#                 result = "0" + result
+#             else:
+#                 result = "1" + result
+#                 carry = "0"
+#             i -= 1
+                
+#         while j >= 0:
+#             if b[j] == carry =="1":
+#                 result = "0" + result
+#                 carry = "1"
+#             elif b[j] == carry =="0":
+#                 result = "0" + result
+#             else:
+#                 result = "1" + result
+#                 carry = "0"
+#             j -= 1
+                
+#         if carry == "1":
+#             result = "1" + result
+        
+#         return result
+
+
+
+
+
+
 s = Solution()
 print(s.addBinary("11","1"))
 print(s.addBinary("1010","1011"))
