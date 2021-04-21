6be757b24b4c7dbfb3a6a06271d3b1fffe6d731b
Bae <qpyu66@naver.com>
2021-04-22 00:00:19 +0900

    lc_100 code refactoring

--- leetcode_easy/lc_100_Same_Tree.py
+++ leetcode_easy/lc_100_Same_Tree.py
@@ -29,6 +29,7 @@ The number of nodes in both trees is in the range [0, 100].
 
 """
 
+#null 처리 못함
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -42,8 +43,7 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        p = TreeNode(p)
-        q = TreeNode(q)
+       
         if (p.val == q.val): 
             if(p.left == q.left or p.right == q.right ):
                 return True
@@ -52,7 +52,46 @@ class Solution(object):
             return False
         else:
             return False
-        
+
+#code refactoring
+#20ms
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution(object):
+#     def isSameTree(self, p, q):
+#         """
+#         :type p: TreeNode
+#         :type q: TreeNode
+#         :rtype: bool
+#         """            
+#         if p == None and q == None:
+#             return True
+#         elif p == None or q == None:
+#             return False
+#         elif p.val == q.val:
+#             return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+
+#         else:
+#             return False
+
+#################12ms
+
+        # if not p and not q:
+        #     return True
+        # if not p or not q:
+        #     return False
+        # if p.val != q.val:
+        #     return False
+        # return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+
+
+
 s = Solution()
 print(s.isSameTree([1,2,3],[1,2,3]))
 #print(s.isSameTree([1,2],[1,null,2]))
