11653f9dcaa178cfea463bb4b43e5583c220bfc9
Bae <qpyu66@naver.com>
2021-04-27 23:49:17 +0900

    lc_108 ok

--- leetcode_easy/lc_108_Convert Sorted Array to Binary Search Tree.py
+++ leetcode_easy/lc_108_Convert Sorted Array to Binary Search Tree.py
@@ -27,3 +27,34 @@ Constraints:
 nums is sorted in a strictly increasing order.
 
 """
+from typing import List
+import time
+start = time.time() 
+#52ms
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+    
+        mid = len(nums) // 2
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right =  self.sortedArrayToBST(nums[mid+1:])
+        return node
+
+
+        
+s = Solution()
+print(s.sortedArrayToBST([-10,-3,0,5,9]))
+print(s.sortedArrayToBST([1,3])) 
+print("time :", time.time() - start)
