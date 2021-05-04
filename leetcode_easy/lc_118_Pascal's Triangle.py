07f7599e537c05d18b59da4a1fce3c881b64791d
Bae <qpyu66@naver.com>
2021-05-05 01:53:19 +0900

    118 code refactoring

--- leetcode_easy/lc_118_Pascal's Triangle.py
+++ leetcode_easy/lc_118_Pascal's Triangle.py
@@ -21,6 +21,7 @@ Constraints:
 1 <= numRows <= 30
 
 """
+from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
@@ -38,6 +39,24 @@ class Solution:
                     n[i].append(n[i][j])
         return n
 
+
+#code refactoring
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 1:
+            return [[1]]
+        
+        res = [[1]]
+        
+        for i in range(2, numRows+1):
+            temp = [1]
+            for j in range(1, i-1):
+                temp.append(res[-1][j-1]+res[-1][j])
+            
+            temp += [1]
+            res.append(temp)
+        
+        return res
 
 
         
