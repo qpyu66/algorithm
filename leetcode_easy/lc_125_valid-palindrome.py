ee8e881ca8ac9c90f37a826e9e09c66db9376a19
Bae <qpyu66@naver.com>
2021-05-13 20:32:51 +0900

    lc 125 ok

--- leetcode_easy/lc_125_valid-palindrome.py
+++ leetcode_easy/lc_125_valid-palindrome.py
@@ -20,4 +20,28 @@ Constraints:
 1 <= s.length <= 2 * 105
 s consists only of printable ASCII characters.
 """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        word1 = ''.join(re.findall("[a-zA-Z0-9]+",s))
+        
+        lowword = word1.lower()
+        
+        if lowword == lowword[::-1]:
+            return True
+        else:
+            return False
 
+
+#code refactoring
+class Solution1:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub("[^a-z0-9]",'', s.lower())
+        return s == s[::-1]
+
+
+
+
+s = Solution()
+#print(s.isPalindrome("A man, a plan, a canal: Panama"))        
+#print(s.isPalindrome("race a car"))    
+print(s.isPalindrome("0P"))   
