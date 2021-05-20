2c09bfea746cf80a012d7958b5cfbd782e47d14a
Bae <qpyu66@naver.com>
2021-05-21 00:11:15 +0900

    lc 145 ok

--- leetcode_easy/lc_145_binary-tree-postorder-traversal.py
+++ leetcode_easy/lc_145_binary-tree-postorder-traversal.py
@@ -34,5 +34,49 @@ The number of the nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 
 """
+from typing import List
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return []
+        return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+    
+#code
+#sample 24 ms submission
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution1:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return None
+        stack = [(root, False)]
+        result = []
+        
+        while stack:
+            curr, res = stack.pop()
+            if res:
+                result.append(curr.val)
+            else:
+                stack.append((curr, True))
+                if curr.right:
+                    stack.append((curr.right, False))
+                if curr.left:
+                    stack.append((curr.left, False))
+        return result
 
 
+    
+s = Solution()
+print(s.preorderTraversal([1,None,2,3]))
+print(s.preorderTraversal([1]))    
+print(s.preorderTraversal([])) 
