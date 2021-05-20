f1584da96b2900a846608e5717cd2515aba57e58
Bae <qpyu66@naver.com>
2021-05-21 00:03:00 +0900

    144 code refactoring

--- leetcode_easy/lc_144_binary-tree-preorder-traversal.py
+++ leetcode_easy/lc_144_binary-tree-preorder-traversal.py
@@ -11,7 +11,7 @@ return [1,2,3].
 Note: Recursive solution is trivial, could you do it iteratively? - see preTraverse_itr
 
 """
-
+from typing import List
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -26,10 +26,37 @@ class Solution:
         return [root.val]+self.preorderTraversal(root.left)+self.preorderTraversal(root.right)
 
 
-
+#code refactoring
+#sample 20 ms submission
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution1:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return None
+        stack = [root]
+        result = []
+        
+        while stack:
+            curr = stack.pop()
+            result.append(curr.val)
+            if curr.right:
+                stack.append(curr.right)
+            if curr.left:
+                stack.append(curr.left)
+        return result
      
     
 s = Solution()
-print(s.preorderTraversal([1,2,2,3]))
+print(s.preorderTraversal([1,None,2,3]))
+"""
+[1] + null + 2
+[2] + 3 + null
+[3] + null + null
+"""
 print(s.preorderTraversal([1]))    
 print(s.preorderTraversal([]))                   
