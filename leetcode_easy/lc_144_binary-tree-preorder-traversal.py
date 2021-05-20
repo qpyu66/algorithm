7ae46a8875fef9e96d07b1c7bd51a19f091d36d2
Bae <qpyu66@naver.com>
2021-05-20 23:54:49 +0900

    lc 144 ok

--- leetcode_easy/lc_144_binary-tree-preorder-traversal.py
+++ leetcode_easy/lc_144_binary-tree-preorder-traversal.py
@@ -12,4 +12,24 @@ Note: Recursive solution is trivial, could you do it iteratively? - see preTrave
 
 """
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        root = TreeNode(root)
+        if root is None:            
+            return []
+        return [root.val]+self.preorderTraversal(root.left)+self.preorderTraversal(root.right)
 
+
+
+     
+    
+s = Solution()
+print(s.preorderTraversal([1,2,2,3]))
+print(s.preorderTraversal([1]))    
+print(s.preorderTraversal([]))                   
