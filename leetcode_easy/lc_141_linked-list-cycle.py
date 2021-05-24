c26baacf79fb1c0ef4121eee537f4b480499e76e
Bae <qpyu66@naver.com>
2021-05-24 22:40:04 +0900

    lc 141 init

--- leetcode_easy/lc_141_linked-list-cycle.py
+++ leetcode_easy/lc_141_linked-list-cycle.py
@@ -37,4 +37,18 @@ pos is -1 or a valid index in the linked-list.
 
 
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        head = ListNode(head)
+        
+        #print(head,head.val,head.next)
+        
+        
+s = Solution()
+print(s.hasCycle([3,2,0,-4]))
