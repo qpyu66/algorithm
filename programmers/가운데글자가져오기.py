26b31081dbaddc16f371193aee6c5223eb1d1916
Bae <qpyu66@naver.com>
2021-05-30 23:52:49 +0900

    가운데글자

---
+++ programmers/가운데글자가져오기.py
@@ -0,0 +1,27 @@
+"""
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+재한사항
+s는 길이가 1 이상, 100이하인 스트링입니다.
+
+s	return
+"abcde"	"c"
+"qwer"	"we"
+
+
+"""
+
+def solution(s):
+    answer = ''
+    for i,v in enumerate(s):
+        #print(i,v)
+        if len(s) % 2 == 1:
+            return s[len(s)//2]
+        else:
+            return s[len(s)//2-1:len(s)//2+1]
+            
+            
+    #return answer
+
+print(solution("abcde"))
+print(solution("qwer"))
