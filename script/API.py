3f9488c3154053dadda6a00f82bfda9b6c9fdc74
Bae <qpyu66@naver.com>
2021-06-02 00:17:55 +0900

    서울에서

---
+++ script/API.py
@@ -0,0 +1,20 @@
+from urllib import request
+import ssl
+import json
+import atexit
+import time
+import datetime
+import pytz
+
+#problemId : [str] 문제번호
+#problemName : [str] 문제이름
+#problemTier : [int] 문제 난이도
+#lastupdate : [float] 업데이트 로그
+
+
+
+
+
+#Example
+if __name__=="__main__":
+    config = dict()
