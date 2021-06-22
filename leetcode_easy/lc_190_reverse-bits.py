e60bd0791dc57a6fa6b2c7aef02ffa78c2126f61
Bae <qpyu66@naver.com>
2021-06-22 21:38:12 +0900

    lc 190 code refactoring

--- leetcode_easy/lc_190_reverse-bits.py
+++ leetcode_easy/lc_190_reverse-bits.py
@@ -36,8 +36,6 @@ The input must be a binary string of length 32
 
 
 
-
-
 #code refactoring
 # class Solution:
 #     # @param n, an integer
@@ -50,7 +48,21 @@ The input must be a binary string of length 32
 #             result |= n&1
 #             n >>=1
 #         return result
-        
+
+
+
+ #code refactoring
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        n = (n >> 16) | (n << 16)
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8)
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4)
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2)
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1)
+        return n    
+
         
 s = Solution()
 print(s.reverseBits(11111111111111111111111111111101))    
