e4d7e1c8fb1120383fe9b066e6eb3fce5818744b
Bae <qpyu66@naver.com>
2021-06-29 23:40:37 +0900

    lc 204

--- leetcode_easy/lc_204_count-primes.py
+++ leetcode_easy/lc_204_count-primes.py
@@ -24,3 +24,23 @@ Constraints:
 
 """
 
+#Time Limit Exceeded
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        count = 0
+        for i in range(2,n):
+            for j in range(2,i):
+                if i%j == 0:
+                    break
+            else:
+                count += 1
+        return count
+
+#Sieve of Eratosthenes 에라토스테네스의 체
+
+
+
+
+s = Solution()
+print(s.countPrimes(10))
+print(s.countPrimes(4))
