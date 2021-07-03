03521d348e4c5cb303b27b1c87b93c02e343a9da
Bae <qpyu66@naver.com>
2021-07-03 21:30:19 +0900

    lc 203

--- leetcode_easy/lc_203_remove-linked-list-elements.py
+++ leetcode_easy/lc_203_remove-linked-list-elements.py
@@ -26,5 +26,36 @@ The number of nodes in the list is in the range [0, 104].
 
 """
 
-
+#pyhon
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+       
+        while head and head.val == val:
+            head = head.next
+        if not head:
+            return head
+        
+        next_node = head
+        while next_node and next_node.next:
+            if next_node.next.val == val:
+                next_node.next = next_node.next.next
+            else:
+                next_node = next_node.next
+                
+        return head
+        
+        
+        
+s=Solution()
+print(s.removeElements([1,2,6,3]))
 
