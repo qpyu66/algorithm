0ddb4bf54dff3f025c3670eeddeeea3bb3a1b2be
Bae <qpyu66@naver.com>
2021-07-08 22:29:16 +0900

    lc 205 code refactoring

--- leetcode_easy/lc_205_isomorphic-strings.py
+++ leetcode_easy/lc_205_isomorphic-strings.py
@@ -28,3 +28,64 @@ t.length == s.length
 s and t consist of any valid ascii character.
 
 """
+
+class Solution:
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        sdict = {}
+        tdict = {}
+        for i in range(len(s)):
+            sarr = s[i]
+            tarr = t[i]
+            print(sarr, tarr)
+
+            if sarr in sdict and sdict[sarr] != tarr:
+                return False
+            if tarr in tdict and tdict[tarr] != sarr:
+                return False
+
+            sdict[sarr] = tarr
+            tdict[tarr] = sarr
+
+        return True
+
+
+
+#code refactoring
+# class Solution:
+#     def isIsomorphic(self, s, t):
+#         """
+#         :type s: str
+#         :type t: str
+#         :rtype: bool
+#         """
+#         m = {}
+#         mapped = set()  # case "ab", "aa"
+#         for i in range(len(s)):
+#             if s[i] not in m and t[i] not in mapped:
+#                 m[s[i]] = t[i]
+#                 mapped.add(t[i])
+#             elif s[i] in m and m[s[i]] == t[i]:
+#                 pass        
+#             else:
+#                 return False
+#         return True
+
+
+#code refactoring
+# class Solution(object):
+#     def isIsomorphic(self, s, t):
+#         print(set(zip(s, t)))
+#         return len(set(zip(s, t))) == len(set(s)) == len(set(t))
+
+
+s = Solution()
+print(s.isIsomorphic("egg","add"))#true
+print(s.isIsomorphic("foo","bar"))   #false
+#print(s.isIsomorphic("paper","title"))  #true
+print(s.isIsomorphic("bbaaba","aabbba"))  #false
+#print(s.isIsomorphic("bbbaaaba","aaabbbba"))  #false
