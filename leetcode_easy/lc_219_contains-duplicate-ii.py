8e59d466219ebf6220b6b348394575c2973408bb
Bae <qpyu66@naver.com>
2021-07-11 00:07:04 +0900

    lc 219 code refactoring

--- leetcode_easy/lc_219_contains-duplicate-ii.py
+++ leetcode_easy/lc_219_contains-duplicate-ii.py
@@ -24,6 +24,49 @@ Constraints:
 0 <= k <= 105
 
 """
+#Time Limit Exceeded
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        print(nums,' k > ',k)
+        
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i] == nums[j]):
+                    if(abs(i-j)<=k):
+                        print('ok > ',abs(i-j))
+                        return True
+                    print('no > ',abs(i-j))
+        return False
 
+#code refactoring
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        if len(list(set(nums))) == len(nums):
+            return False
+        
+        for i in range(len(nums)):
+            for j in range(1, k+1):
+                if i+j > len(nums) -1:
+                    continue
+                if nums[i] == nums[i+j]:
+                    return True
+                
+        return False
+    
+        
+s = Solution()
+print(s.containsNearbyDuplicate([1,2,3,1],3)) #True
+print(s.containsNearbyDuplicate([1,1,0,1],1)) #True
+print(s.containsNearbyDuplicate([1,2,3,1,2,3],2)) #False
 
 
