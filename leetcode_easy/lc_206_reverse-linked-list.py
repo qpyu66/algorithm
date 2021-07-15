46822ef6976573f0904d0b6c01de0544157ce4cc
Bae <qpyu66@naver.com>
2021-07-15 23:49:44 +0900

    206 solution

--- leetcode_easy/lc_206_reverse-linked-list.py
+++ leetcode_easy/lc_206_reverse-linked-list.py
@@ -28,3 +28,23 @@ The number of nodes in the list is the range [0, 5000].
 Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
 
 """
+
+
+#solution
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev_node = None
+        curr_node = head
+        while curr_node:
+            next_node = curr_node.next # Remember next node
+            curr_node.next = prev_node  # REVERSE! None, first time round.
+            prev_node = curr_node  # Used in the next iteration.
+            curr_node = next_node  # Move to next node.
+        head = prev_node
+        return head
+
+        
